9a5366a9630e652648fb4eb4bdd30b1daae72674
David Woo <david@familywoo.com>
2021-12-04 16:29:21 +0000

    day3 part1

--- aoc2021/day3/day3.cpp
+++ aoc2021/day3/day3.cpp
@@ -2,10 +2,52 @@
 //
 
 #include <iostream>
+#include <bitset>
+#include <vector>
+#include <format>
+#include <ranges>
+#include <algorithm>
+#include <array>
+
+import input_lib;
+
+static constexpr size_t BIT_WIDTH = 12;
+
+using report_bits = std::bitset<BIT_WIDTH>;
+
+report_bits string_to_bits(const std::string& s)
+{
+    return report_bits{ s };
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //auto report = input::read_vector("../input_files/day3_example.txt", string_to_bits);
+    auto report = input::read_vector("../input_files/day3.txt", string_to_bits);
+
+    std::array<int, BIT_WIDTH> counts{};
+
+    std::ranges::for_each(report, [&counts](const auto& bits)
+        {
+            for (int i = 0; i < BIT_WIDTH; ++i)
+            {
+                counts[i] += bits[i] ? 1 : -1;
+            }
+
+        });
+
+    report_bits gamma{};
+
+    for (int i = 0; i < BIT_WIDTH; ++i)
+    {
+        gamma[i] = counts[i] > 0;
+    }
+
+    auto gdec = gamma.to_ullong();
+
+    report_bits epsilon = gamma.flip();
+    auto answer = gdec * epsilon.to_ullong();
+    std::cout << std::format( "The answer is {}\n", answer);
 }
 
 
