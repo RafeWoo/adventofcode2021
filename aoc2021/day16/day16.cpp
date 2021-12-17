fa8d03c8d06864cda7ce8dfaea8eceee7786030e
David Woo <david@familywoo.com>
2021-12-17 22:53:22 +0000

    day16 start

---
+++ aoc2021/day16/day16.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <ranges>
+#include <string_view>
+#include <iomanip>
+#include <algorithm>
+#include <format>
+#include <set>
+#include <map>
+#include <limits>
+#include <array>
+#include <stack>
+#include <numeric>
+#include <assert.h>
+#include <unordered_map>
+#include <regex>
+
+import input_lib;
+
+int main(void)
+{
+	return 0;
+}
