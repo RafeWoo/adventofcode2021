648dceafcff1d1fab5ad70e82438cf492853ffe8
David Woo <david@familywoo.com>
2021-12-19 20:21:00 +0000

    day15 start

---
+++ aoc2021/day15/day15.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <ranges>
+#include <string_view>
+#include <iomanip>
+#include <algorithm>
+#include <format>
+#include <set>
+#include <map>
+#include <limits>
+#include <array>
+#include <stack>
+#include <numeric>
+#include <assert.h>
+#include <unordered_map>
+#include <iterator>
+#include <bitset>
+#include <functional>
+
+import input_lib;
+
+
+int main(void)
+{
+	return 0;
+}
