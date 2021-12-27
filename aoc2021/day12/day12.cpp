9767b793ac3c02b968ac671f98012a10c881dcc9
David Woo <david@familywoo.com>
2021-12-27 20:53:18 +0000

    day12 start

---
+++ aoc2021/day12/day12.cpp
@@ -0,0 +1,21 @@
+
+#include <ranges>
+#include <string>
+#include <unordered_map>
+
+import input_lib;
+
+
+class Graph
+{
+
+	//for each vertex we have a list of edges
+
+	//string ids a vertex
+
+};
+
+int main(void)
+{
+	return 0;
+}
